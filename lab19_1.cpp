db69b699f485d6079e0f6fa12f98986031f95731
napapon-wongkom <dot.wongkom@gmail.com>
2023-02-28 13:54:11 +0700

    lab19_1.cpp

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,76 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &name,vector<int> &score,vector<char> &grade){
+    ifstream source;
+    source.open(filename);
+    string textline;
+    char format[] = "%[^:]: %d %d %d";
+    int a,b,c;
+    char names[100];
+    while(getline(source,textline)){
+        sscanf(textline.c_str(),format,names,&a,&b,&c);
+        name.push_back(names);
+        int sum = a+b+c;
+        score.push_back(sum);
+        grade.push_back(score2grade(sum));
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    cout<<"Please input your command: ";
+    string text;
+    char comm[100],keys[100];
+    getline(cin,text);
+    if(toUpperStr(text)=="EXIT"){
+        command = text;
+    }else{
+        text.insert(text.size(),",");
+        char format_text[] = "%[^ ,] %[^,],";
+        sscanf(text.c_str(),format_text,comm,keys);
+        command = comm;
+        key = keys;
+    }
 }
 
-void searchName(){
-
+void searchName(vector<string> name,vector<int> score,vector<char> grade, string key){
+    int count=0;
+    string KEY = toUpperStr(key);
+    
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < score.size(); i++){
+        string NAME = toUpperStr(name[i]);
+        if(NAME==KEY){
+            cout<<name[i]<<"\'s score = "<<score[i]<<"\n";
+            cout<<name[i]<<"\'s grade = "<<grade[i]<<"\n";
+            count = count + 1;
+        } 
+    }
+    if(count==0){
+            cout<<"Cannot found.\n";
+        }
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> name,vector<int> score,vector<char> grade, string key){
+    int count=0;
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < score.size(); i++)
+    {
+        
+        string grade_s;
+        grade_s.push_back(grade[i]);
+        if(grade_s==key){
+            cout<<name[i]<<" ("<<score[i]<<")\n";
+            count += 1;
+            
+        }
+        
+    }
+    if(count==0){
+            cout<<"Cannot found.\n";
+        }
+    cout << "---------------------------------\n";
 }
 
 
